04d5963e042b1ea5d5c56f73ebbf696fc0e1da30
LunaAmora <luna.mberry@gmail.com>
2024-11-28 13:35:08 -0300

    Add mold linker to rust

--- home/default.nix
+++ home/default.nix
@@ -21,7 +21,14 @@
       rustup
       gcc
       nil
+      mold
     ];
+
+    file.".cargo/config.toml".text = ''
+      [target.x86_64-unknown-linux-gnu]
+      linker = "clang"
+      rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
+    '';
   };
 
   programs.bash.historyControl = [
