13af9b8a6d01c72b711a1c6b101413c173a7bc9b
LunaAmora <luna.mberry@gmail.com>
2024-12-12 20:40:47 -0300

    Add wallpaper to sddm login

--- modules/wm/kde.nix
+++ modules/wm/kde.nix
@@ -1,10 +1,30 @@
 { pkgs, ... }:
 
+let
+  background-package = pkgs.stdenvNoCC.mkDerivation {
+    name = "background-image";
+    src = ./.;
+    dontUnpack = true;
+    installPhase = ''
+      cp $src/wallpaper.png $out
+    '';
+  };
+in
 {
   services = {
-    displayManager.sddm.enable = true;
+    displayManager.sddm = {
+      enable = true;
+      theme = "breeze";
+    };
     desktopManager.plasma6.enable = true;
   };
+
+  environment.systemPackages = [
+    (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
+      [General]
+      background = ${background-package}
+    '')
+  ];
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     elisa
