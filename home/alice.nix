dcfaecbd22c2d17b0ac9ad6925c1037e939fb144
LunaAmora <luna.mberry@gmail.com>
2026-03-01 19:45:07 -0300

    Add catppuccin to alice config

--- home/alice.nix
+++ home/alice.nix
@@ -1,4 +1,11 @@
 {pkgs, ...}: {
+  programs.vscode = {
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        catppuccin.catppuccin-vsc
+      ];
+    };
+  };
 
   home = {
     packages = with pkgs; [
